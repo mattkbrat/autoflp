2d8d830c771ef16ca3ee98182e8247bffc38f966
John Doe <johndoe@example.com>
2023-09-16 09:08:23 -0600

    Add docker for deployment

--- nextjs/lib/paths.ts
+++ nextjs/lib/paths.ts
@@ -1,10 +1,13 @@
 export const rootPath = process.cwd();
 
+console.log('rootPath', rootPath);
+
 // /lib/forms
-export const inputPath = `${rootPath}/lib/forms/`;
+export const inputPath = `/forms`;
 
 // /public/forms/filled
-export const formsPath = `${rootPath}/public/forms/filled/`;
+// export const formsPath = `${rootPath}/public/forms/filled/`;
+export const formsPath = `${inputPath}/filled/`;
 
 export const outputFullPath = (output: string) =>
   `${formsPath}${output.includes('.pdf') ? output : output + '.pdf'}`;
--- nextjs/next.config.js
+++ nextjs/next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  output: 'standalone',
+  swcMinify: true,
   experimental: {
     appDir: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
   },
   images: {
     domains: ['localhost', 'images.unsplash.com'],
