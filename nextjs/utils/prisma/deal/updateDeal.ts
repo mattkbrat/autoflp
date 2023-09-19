ed24ea8700e16a5419e28f07abeacb209d25ca49
Matt <mattkbrat@pm.me>
2023-09-19 11:36:46 -0600

    Send a notification when a deal is updated

--- nextjs/utils/prisma/deal/updateDeal.ts
+++ nextjs/utils/prisma/deal/updateDeal.ts
@@ -1,21 +1,35 @@
 import prisma from '@/lib/prisma';
 import { Deal } from '@/types/prisma/deals';
+import notifyDeal from '@/utils/pushover/deal';
+import { fullNameFromPerson } from '@/utils/format/fullNameFromPerson';
+import formatInventory from '@/utils/format/formatInventory';
 
 const updateDeal = async ({ id, data }: { id: string; data: Partial<Deal> }) => {
-  return await prisma.deal.update({
-    where: {
-      id,
-    },
-    include: {
-      inventory: true,
-      Account: {
-        select: {
-          person: true,
+  return await prisma.deal
+    .update({
+      where: {
+        id,
+      },
+      include: {
+        inventory: true,
+        Account: {
+          select: {
+            person: true,
+          },
         },
       },
-    },
-    data,
-  });
+      data,
+    })
+    .then(async (deal) => {
+      await notifyDeal({
+        amount: +deal.cash,
+        dealId: deal.id,
+        type: data.state === 0 ? 'CLOSE' : 'UPDATE',
+        fullName: fullNameFromPerson(deal.Account.person),
+        invString: formatInventory(deal.inventory),
+      });
+      return deal;
+    });
 };
 
 export default updateDeal;
